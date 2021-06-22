f5c0765787051b7e4cb205c145b4034668cfcc12
Ralph Urlus <rurlus@Ralphs-MacBook-Pro.local>
2021-06-22 15:05:19 +0200

    Set version of extension using enviroment variable

--- phik/simcore/bindings.cpp
+++ phik/simcore/bindings.cpp
@@ -1,6 +1,15 @@
 #include <pybind11/pybind11.h>
 #include "simulation.hpp"
 
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
 PYBIND11_MODULE(phik_simulation_core, m) {
     bind_simulation(m);
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
 }
