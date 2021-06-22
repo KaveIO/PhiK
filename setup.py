2f3b002c5ac5894ff68ece037cdfbe2dcfb25c8f
Ralph Urlus <rurlus@Ralphs-MacBook-Pro.local>
2021-06-22 15:03:20 +0200

    Use skbuild for extension

--- setup.py
+++ setup.py
@@ -12,25 +12,23 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted according to the terms listed in the file
 LICENSE.
 """
-
+from __future__ import print_function
 import os
 import sys
-import subprocess
+import shutil
 from warnings import warn
+from setuptools import setup
 from setuptools import find_packages
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 from distutils.errors import CompileError, DistutilsExecError, DistutilsPlatformError
-import pybind11
 
 ext_errors = (
+    ImportError,
     CompileError,
     DistutilsExecError,
     DistutilsPlatformError,
     IOError,
     SystemExit
 )
-
 
 NAME = 'phik'
 
@@ -72,102 +70,10 @@ if DEV:
     REQUIREMENTS += TEST_REQUIREMENTS
 
 
-# Convert distutils Windows platform specifiers to CMake -A arguments
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
-
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
-
-        cfg = "Debug" if self.debug else "Release"
-
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
-        cmake_args = [
-            "-Dpybind11_DIR={}".format(pybind11.get_cmake_dir()),
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
-            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
-            "-DPHIK_VERSION_INFO={}".format(self.distribution.get_version()),
-            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
-        ]
-        build_args = []
-
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator:
-                cmake_args += ["-GNinja"]
-
-        else:
-
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-                ]
-                build_args += ["--config", cfg]
-
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += ["-j{}".format(self.parallel)]
-
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
-        subprocess.check_call(
-            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
-        )
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-        )
-
 COMMAND_OPTIONS = dict()
 EXCLUDE_PACKAGES = []
-CMD_CLASS = {"build_ext": CMakeBuild}
-EXTERNAL_MODULES = [CMakeExtension('phik_simulation_core')]
+CMD_CLASS = {}
+EXTERNAL_MODULES = []
 
 # read the contents of readme file
 with open("README.rst", encoding="utf-8") as f:
@@ -209,6 +115,7 @@ setup_args = {
     'long_description': long_description,
     'long_description_content_type': "text/x-rst",
     'python_requires': '>=3.6',
+    'cmake_install_dir': 'phik/lib',
     'packages': find_packages(exclude=EXCLUDE_PACKAGES),
     # Setuptools requires that package data are located inside the package.
     # This is a feature and not a bug, see
@@ -246,8 +153,10 @@ setup_args = {
 if __name__ == '__main__':
     write_version_py()
     try:
+        import skbuild
+        os.environ["PHIK_VERSION_INFO"] = setup_args['version']
         # try building with C++ extension:
-        setup(ext_modules=EXTERNAL_MODULES, **setup_args)
+        skbuild.setup(**setup_args)
     except ext_errors as ex:
         warn(
             '\n---------------------------------------------\n'
@@ -256,12 +165,17 @@ if __name__ == '__main__':
             f'{ex.__class__.__name__} {ex.__str__()}\n\n'
             '\n---------------------------------------------\n'
         )
-
-        ## Retry to install the module without extension :
-        # Remove any previously defined build_ext command class.
-        if 'build_ext' in setup_args['cmdclass']:
-            del setup_args['cmdclass']['build_ext']
-
+        # Retry to install the module without extension
+        setup_args.pop('cmake_install_dir')
         # If this new 'setup' call doesn't fail, the module
         # will be successfully installed, without the C++ extension :
         setup(**setup_args)
+
+    # clean up the build directory when doing an inplace build as this
+    # causes issues with CMake cache if rerun
+    build_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '_skbuild'
+    )
+    if os.path.isdir(build_dir):
+        shutil.rmtree(build_dir)
