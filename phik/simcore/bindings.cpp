beae0de6351f70740e0a72002ae0db480e5de648
Ralph Urlus <rurlus.dev@gmail.com>
2023-12-23 22:37:24 +0100

    BLD: [CPP] Switch to scikit-build-core as build system

--- phik/simcore/bindings.cpp
+++ phik/simcore/bindings.cpp
@@ -1,6 +1,4 @@
-#include <pybind11/pybind11.h>
 #include "simulation.hpp"
+#include <pybind11/pybind11.h>
 
-PYBIND11_MODULE(phik_simulation_core, m) {
-    bind_simulation(m);
-}
+PYBIND11_MODULE(_phik_simulation_core, m) { bind_simulation(m); }
