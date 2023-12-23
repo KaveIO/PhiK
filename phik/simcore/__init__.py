4a7bcacbde64e2467a6e7d54491bb0e24fe24557
Ralph Urlus <rurlus.dev@gmail.com>
2023-12-23 22:37:24 +0100

    BLD: [CPP] Switch to scikit-build-core as build system

--- phik/simcore/__init__.py
+++ phik/simcore/__init__.py
@@ -1,3 +1,3 @@
-from phik.lib.phik_simulation_core import _sim_2d_data_patefield
+from phik.lib._phik_simulation_core import _sim_2d_data_patefield
 
-__all__ = ['_sim_2d_data_patefield']
+__all__ = ["_sim_2d_data_patefield"]
